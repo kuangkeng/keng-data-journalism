b169a84b1e3e6e0ea36daf25b02ee174fcc6cc47
Kuang Keng Kuek Ser <kuangkeng@gmail.com>
2014-10-06 23:48:58 -0400

    sankeygreenhouse.json renamed to data.json

--- timeuseform/nochart.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('a[data-text]').each(function(){
-      $(this).attr('data-text', "This works!");
-    });
-    $.getScript('http://platform.twitter.com/widgets.js');
-});
